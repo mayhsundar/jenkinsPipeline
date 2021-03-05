87e3a761ead8e117abab60c69d8f18cf3be60349
mayhsundar <shyam-sundar.yadav@capgemini.com>
2021-03-05 20:52:13 +0530

    first maven project

---
+++ src/test/java/jenkinsPipeline/ShowMeOut.java
@@ -0,0 +1,12 @@
+package jenkinsPipeline;
+
+public class ShowMeOut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Name: Jenkins Pipeline");
+
+	}
+
+}
